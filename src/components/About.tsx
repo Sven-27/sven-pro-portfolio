c2fbc525e2ed3cbb1acdd6c4b324f39496f31be0
Sven Notermans <sven.notermans@live.nl>
2023-01-01 15:23:58 +0100

    finished about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,8 +5,13 @@ type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div className=" flex flex-col md:flex-row relative h-screen text-center md:text-left
-    max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className=" flex flex-col md:flex-row relative h-screen text-center md:text-left
+      max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
@@ -26,7 +31,24 @@ const About = (props: Props) => {
       className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
       md:rounded-lg md::w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
-    </div>
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a {" "}
+          <span className="underline decoration-[#F7ABBA]/50">little</span>{" "}
+           background
+         </h4>
+        <p className="text-base">
+          Hi, I am Sven Notermans. I am from the Netherlands and it is my pleasure to meet you! 
+          I like to create projects that are simple and sleek, at the same time I make sure that what I do is of high quality. 
+          What I love about frontend development is the fact that it's always moving. 
+          Every day is different and because every day is different, the challenges I face are also different, 
+          which challenges me to find solutions. That helped me to learn how to think in solutions. 
+          Not just with development but in all aspects of my life. So it greatly improved the quality of my life. 
+          As a result, I have also grown as an individual. 
+          That&rsquo;s what development does for me and that&rsquo;s why I do it with heart and soul.
+        </p>
+      </div>
+    </motion.div>
   )
 }
 
