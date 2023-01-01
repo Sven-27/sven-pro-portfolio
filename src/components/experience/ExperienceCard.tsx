8190c2b97eec98f81adec435e4521de489f8d5d6
Sven Notermans <sven.notermans@live.nl>
2023-01-01 16:32:57 +0100

    working on experience

--- src/components/experience/ExperienceCard.tsx
+++ src/components/experience/ExperienceCard.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
+import author from '../../../public/media/images/author.jpg';
+
 
 type Props = {}
 
 const ExperienceCard = (props: Props) => {
   return (
-    <article>
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0
+    w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10" 
+    >
       <motion.img
         initial={{
           x: -100,
@@ -21,6 +26,40 @@ const ExperienceCard = (props: Props) => {
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQva_sHsMKH0SirWFOulRg4Ve0I4cyetMzBIA&usqp=CAU"
         alt="image"
       />
+      <section className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">Frontend Engineer</h4>
+        <p className="font-bold text-2xl mt-1">Bee Ideas</p>
+        <div className="flex space-x-2 my-2">
+          <Image 
+            src={ author }
+            alt=""
+            className="h-10 w-10 rounded-full"
+          />
+          <Image 
+            src={ author }
+            alt=""
+            className="h-10 w-10 rounded-full"
+          />
+          <Image 
+            src={ author }
+            alt=""
+            className="h-10 w-10 rounded-full"
+          />
+          <Image 
+            src={ author }
+            alt=""
+            className="h-10 w-10 rounded-full"
+          />
+        </div>
+        <p className="uppercase py-5 text-gray-300">Started work... - Ended work...</p>
+        <ul className="list-disc space-y-4 ml-5 text-lg">
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+        </ul>
+      </section>
     </article>
   )
 }
